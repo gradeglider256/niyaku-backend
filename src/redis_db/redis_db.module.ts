d2b188d9e6fcdaaadaf16fd6a0da420690ebf54f
Patrick Aiko <patrick.aikonia@gmail.com>
2025-12-20 08:51:03 +0300

    a few changes to docker

--- src/redis_db/redis_db.module.ts
+++ src/redis_db/redis_db.module.ts
@@ -6,29 +6,29 @@ import KeyvRedis from '@keyv/redis';
 
 @Global()
 @Module({
-    imports: [
-        CacheModule.registerAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: async (configService: ConfigService) => {
-                const password = configService.get<string>('REDIS_PASSWORD');
-                const host = configService.get<string>('REDIS_HOST') ?? 'localhost';
-                const port = configService.get<number>('REDIS_PORT') ?? 6379;
-                const redisUrl = password
-                    ? `redis://:${password}@${host}:${port}`
-                    : `redis://${host}:${port}`;
+  imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const password = configService.get<string>('REDIS_PASSWORD');
+        const host = configService.get<string>('REDIS_HOST') ?? 'localhost';
+        const port = configService.get<number>('REDIS_PORT') ?? 6379;
+        const redisUrl = password
+          ? `redis://:${password}@${host}:${port}`
+          : `redis://${host}:${port}`;
 
-                const keyv = new Keyv({
-                    store: new KeyvRedis(redisUrl),
-                });
+        const keyv = new Keyv({
+          store: new KeyvRedis(redisUrl),
+        });
 
-                return {
-                    store: keyv,
-                    ttl: 600000, // 10 minutes in milliseconds
-                };
-            },
-        }),
-    ],
-    exports: [CacheModule],
+        return {
+          store: keyv,
+          ttl: 600000, // 10 minutes in milliseconds
+        };
+      },
+    }),
+  ],
+  exports: [CacheModule],
 })
-export class RedisDbModule { }
+export class RedisDbModule {}
